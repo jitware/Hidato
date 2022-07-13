ff6a225a6b734b6cba9a42cc487a8ebdf4b9687f
manu-tg <emmanueltorrestdad@gmail.com>
2022-07-13 02:57:16 -0400

    fix validNeigbors

--- src/Solver.hs
+++ src/Solver.hs
@@ -10,16 +10,19 @@ import Lib
 ---- xs: La lista que se va a iterar
 ---- end: la tupla final
 ---- Value: valor del siguiente vecino
----- boxes: son las casillas marcadas
-myfold_bool f matrix acc xs  end value boxes  = aux matrix acc xs  end value boxes
-  where  aux matrix acc []  end value boxes     = acc
-        --  aux matrix (True, _) _  end value boxes = acc
-         aux matrix acc (x:xs)  end value boxes = aux matrix (f matrix acc x end value boxes) xs  end value boxes
+
+tLoop f matrix acc xs  end value     = aux matrix acc xs  end value 
+  where  aux matrix acc []  end value      = acc
+         aux matrix acc (x:xs)  end value  = if (fst acc) == True
+                then acc
+                else aux matrix (f matrix acc x end value ) xs  end value 
 
 -- Agrega los valores unicos
-changeMatrix matrix value neigbor = 
-    (setInMatrix matrix value (neigbor!!0) (neigbor!!1))
-    -- fullMatrixUniques ((setInMatrix matrix value (neigbor!!0) (neigbor!!1)))
+changeMatrix matrix value neigbor = setInMatrix matrix value (neigbor!!0) (neigbor!!1)
+
+-- changeMatrix :: [[Int]]->Int->[Int]->[[Int]]
+-- changeMatrix matrix value neigbor =  
+--     fullMatrixUniques ((setInMatrix matrix value (neigbor!!0) (neigbor!!1)))
 
 updateEnd :: [[Int]]->Int->[Int]
 updateEnd matrix n = 
@@ -28,32 +31,43 @@ updateEnd matrix n =
         then []
         else boxes!!0
 
-backtraking :: [Int] -> [Int] -> [[Int]] -> [[Int]] ->(Bool, [[Int]])
+backtraking :: [Int] -> [Int] -> [[Int]] ->(Bool, [[Int]])
 backtraking current end matrix = 
     -- Comprueba que haya llegado al objetivo por el camino correcto 
     -- sino regresa hacia atrás y se mueve por otra casilla
+    
     if index matrix current  == index matrix end
         then if current == end
-                then    let next_end = updateEnd matrix ((index matrix end)+1)
+                then  let next_end = updateEnd matrix ((index matrix end)+1)
                         in if next_end == []
                             then (True, matrix)
+                            -- else  (True, matrix)
                             else backtraking end next_end matrix                
                 else (False, [])
-        else
-            ---si mi sucesor es el end buscalo en sus vecinos
-            let neigbors = validNeigbors current matrix
-                in myfold_bool fun matrix (False, []) neigbors end ((index matrix current)+1)
+        else let neigbors = validNeigbors current matrix
+            in tLoop fun matrix (False, []) neigbors end ((index matrix current)+1)
                 where
-                    fun matrix acc neigbor end value  = if (fst acc) == True
-                        then acc
-                        else let newMatrix = (changeMatrix matrix value neigbor)
-                             -- new end = updateEnd newMatrix value
-                             in backtraking neigbor end newMatrix
+                    fun matrix acc neigbor end value  =
+                            let newMatrix = changeMatrix matrix value neigbor 
+                                -- newEnd = updateEnd newMatrix value
+                                newEnd = end
+                            in 
+                                if newEnd /= []
+                                    then backtraking neigbor newEnd newMatrix
+                                    else (True,matrix)
 
 solve matrix = 
     let current = posUno matrix
-        end = updateEnd matrix 2 
-        in backtraking current end matrix
+        newMatrix = fullMatrixUniques matrix
+        end = updateEnd newMatrix 1
+    in backtraking current end newMatrix
 
+--- let matrix = [[0, 0  , 0,  0, 0,  0,  0 ],[0, 0  , 1,  0, 0,  0, 0 ],[0, 0 , 0,  0, 0,  0, 0 ],[0, 0 , 0,  0, 0,  0, 0 ],[0, 0 , 0,  0, 40, 0,  0 ],[0, 0  , 0,  0, 0,  0,  0 ],[0, 0  , 0,0,0,  0,  0 ]]
+--- let matrix = [[1,0,-1,6],[0,-1,0,0]]
+---  let matrix = [[1,2,-1,6],[2,-1,0,0]]
+-- Caso 1
+-- let matrix = [[1,2,-1,6],[3,-1,0,0]]
 -- let matrix =   [[0  ,33 ,35 ,0  ,0  ,-1 ,-1 ,-1 ], [0  ,0  ,24 ,22 ,0  ,-1 ,-1 ,-1 ], [0  ,0  ,0  ,21 ,0  ,0  ,-1 ,-1 ], [0  ,26 ,0  ,13 ,40 ,11 ,-1, -1 ], [27 ,0  ,0  ,0  ,9  ,0  ,1  ,-1 ], [-1 ,-1 ,0  ,0  ,18 ,0  ,0  ,-1 ], [-1 ,-1 ,-1 ,-1 ,0  ,7  ,0  ,0  ], [-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,5  ,0  ]]
 -- solve matrix
+-- changeMatrix matrix 1 [0,1]
+-- let new = fullMatrixUniques matrix 
